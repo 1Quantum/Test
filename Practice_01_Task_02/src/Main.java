2ec2c89afbcefc6c9c783e2086de745316a241dc
Dastan <Dastan@example.com>
2018-06-21 16:32:55 +0600

    Initial commt

---
+++ Practice_01_Task_02/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("**************");
+       System.out.println("*Привет, Мир!*");
+       System.out.println("**************");
+
+
+
+//    public static void main (String[] args) {
+//        System.out.println("**************\n*Привет, Мир!*\n**************");
+
+
+
+//    public static void main(String[] args) {
+//          System.out.println(
+
+//                  "**************\n" +
+//                  "*Привет, Мир!*\n" +
+//                  "**************"
+//          );
+
+
+    }
+}
+
